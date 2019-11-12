e15265959fbdaf9f7f0655560ef56e168bd9009d
Brandon <brandonjamiller@hotmail.com>
2019-11-12 18:46:50 +0000

    complete section 5.3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def new
   end
+
+  def create
+    render plain: params[:article].inspect
+  end
 end
