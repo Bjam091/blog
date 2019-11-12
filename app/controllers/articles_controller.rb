781c0a9bdde0f4c2117d79dfb72e7d50b9546667
Brandon <brandonjamiller@hotmail.com>
2019-11-12 18:59:52 +0000

    complete section 5.7

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,9 +5,17 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to @article
   end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+   
+  def new
+  end
    
   private
     def article_params
       params.require(:article).permit(:title, :text)
     end
+
 end
